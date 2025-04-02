43ed858a838478f506008b5b4a282aacf587d9a4
Emre Gemici <35531700+emrgem@users.noreply.github.com>
2025-04-02 17:39:43 +0000

    selector practice

---
+++ Unit5-DOM/Lesson02-Element_Selectors/practice1/script.js
@@ -0,0 +1,8 @@
+Brisk Logo Menu
+// Select all <div> elements with class "box" and:
+// change background color to "lightblue"
+
+
+
+// Select <ul> element with ID "list" and 
+//change: text color of all <li> children to "green"
---
+++ Unit5-DOM/Lesson02-Element_Selectors/practice2/script.js
@@ -0,0 +1,6 @@
+// Select the <h2> element with ID "heading" and:
+// change its text content as 'Updated Heading'
+
+
+// Select all <p> elements inside <div> with class "content" and:
+// change: font color to red, fontWeight as bold, font size to 18px
