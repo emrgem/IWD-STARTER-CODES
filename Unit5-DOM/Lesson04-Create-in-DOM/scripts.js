cfcc8cefe7c471f9dd6999df023551fbf0580c39
Emre Gemici <35531700+emrgem@users.noreply.github.com>
2025-04-11 14:00:46 -0400

    Create scripts.js

---
+++ Unit5-DOM/Lesson04-Create-in-DOM/scripts.js
@@ -0,0 +1,13 @@
+function addCard() {
+  const cardContainer = document.getElementById("cardContainer");
+  const card = document.createElement("div");
+  card.innerHTML = `<div class="card m-2" style="width: 18rem;">
+      <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>`;
+  cardContainer.appendChild(card);
+}
